11eaa02f72a9e1ff9e48991a187b4131b49d9434
Fábio Mendes <43360747+mendesfabio@users.noreply.github.com>
2022-09-24 11:25:03 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+mod abi;
+mod pb;
+use hex_literal::hex;
+use pb::erc721;
+use substreams::{log, store, Hex};
+use substreams_ethereum::{pb::eth::v2 as eth, NULL_ADDRESS};
+
+// Bored Ape Club Contract
+const TRACKED_CONTRACT: [u8; 20] = hex!("bc4ca0eda7647a8ab7c2061c2e118a18a936f13d");
+
+substreams_ethereum::init!();
+
+/// Extracts transfers events from the contract
+#[substreams::handlers::map]
+fn map_transfers(blk: eth::Block) -> Result<erc721::Transfers, substreams::errors::Error> {
+    Ok(erc721::Transfers {
+        transfers: blk
+            .events::<abi::erc721::events::Transfer>(&[&TRACKED_CONTRACT])
+            .map(|(transfer, log)| {
+                substreams::log::info!("NFT Transfer seen");
+
+                erc721::Transfer {
+                    trx_hash: log.receipt.transaction.hash.clone(),
+                    from: transfer.from,
+                    to: transfer.to,
+                    token_id: transfer.token_id.low_u64(),
+                    ordinal: log.block_index() as u64,
+                }
+            })
+            .collect(),
+    })
+}
+
+/// Store the total balance of NFT tokens for the specific TRACKED_CONTRACT by holder
+#[substreams::handlers::store]
+fn store_transfers(transfers: erc721::Transfers, s: store::StoreAddInt64) {
+    log::info!("NFT holders state builder");
+    for transfer in transfers.transfers {
+        if transfer.from != NULL_ADDRESS {
+            log::info!("Found a transfer out {}", Hex(&transfer.trx_hash));
+            s.add(transfer.ordinal, generate_key(&transfer.from), -1);
+        }
+
+        if transfer.to != NULL_ADDRESS {
+            log::info!("Found a transfer in {}", Hex(&transfer.trx_hash));
+            s.add(transfer.ordinal, generate_key(&transfer.to), 1);
+        }
+    }
+}
+
+fn generate_key(holder: &Vec<u8>) -> String {
+    return format!("total:{}:{}", Hex(holder), Hex(TRACKED_CONTRACT));
+}
