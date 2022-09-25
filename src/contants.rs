b84fc7a1c5b041513a1a4f6d9321c9005d3772e7
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-09-25 13:20:17 -0300

    create constants mod

---
+++ src/contants.rs
@@ -0,0 +1,15 @@
+use hex_literal::hex;
+
+type Address = [u8; 20];
+
+pub const VAULT_ADDRESS: Address = hex!("ba12222222228d8ba445958a75a0704d566bf2c8");
+
+const WEIGHTED_V2_FACTORY: Address = hex!("cc508a455f5b0073973107db6a878ddbdab957bc");
+
+#[allow(dead_code)]
+pub fn get_pool_type_from_factory(address: Address) -> &'static str {
+    match address {
+        WEIGHTED_V2_FACTORY => return "Weighted",
+        _ => return "Unknown",
+    }
+}
