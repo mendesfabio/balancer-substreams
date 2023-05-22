534a98c125da3c1c548ba9ffc5adf485d7fa988e
Fábio Mendes <ms.fabiomendes@gmail.com>
2023-05-22 15:05:37 -0300

    remove unused abis

--- build.rs
+++ build.rs
@@ -2,18 +2,9 @@ use anyhow::{Ok, Result};
 use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), anyhow::Error> {
-    Abigen::new("WeightedPool", "abi/pool.json")?
-        .generate()?
-        .write_to_file("src/abi/pool.rs")?;
-    Abigen::new("WeightedPoolFactory", "abi/factory.json")?
-        .generate()?
-        .write_to_file("src/abi/factory.rs")?;
     Abigen::new("Vault", "abi/vault.json")?
         .generate()?
         .write_to_file("src/abi/vault.rs")?;
-    Abigen::new("ERC20", "abi/erc20.json")?
-        .generate()?
-        .write_to_file("src/abi/erc20.rs")?;
 
     Ok(())
 }
--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -1,11 +1,2 @@
 #[allow(unused_imports)]
-pub mod erc20;
-
-#[allow(unused_imports)]
-pub mod factory;
-
-#[allow(unused_imports)]
-pub mod pool;
-
-#[allow(unused_imports)]
 pub mod vault;
