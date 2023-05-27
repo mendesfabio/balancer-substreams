7ef15328bc32bef904e69f28ee3ae024fb251970
Fábio Mendes <ms.fabiomendes@gmail.com>
2023-05-27 14:41:56 -0300

    add erc20 generated abi

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ use anyhow::{Ok, Result};
 use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), anyhow::Error> {
+    Abigen::new("ERC20", "abi/erc20.json")?
+        .generate()?
+        .write_to_file("src/abi/erc20.rs")?;
     Abigen::new("Vault", "abi/vault.json")?
         .generate()?
         .write_to_file("src/abi/vault.rs")?;
--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -1,2 +1,5 @@
 #[allow(unused_imports)]
+pub mod erc20;
+
+#[allow(unused_imports)]
 pub mod vault;
